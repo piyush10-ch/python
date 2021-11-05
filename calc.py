9018f9b9d2d7807c15f1a94fbd5ce969c6799578
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-11-05 07:30:32 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,12 @@
+print(2+3)
+print(9%2)
+print(25/2)
+print(25//2)
+print(2**9)
+print(25/2%6)
+print(16**0.5)
+print(2**0.5)
+print(round(2**0.5,4) )#round function is used to give only number answer which we want
+print(2**3/2*6-4*(3-4/2))
+print(2**3**2)
+
