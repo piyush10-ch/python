8c1274822b6f3e7cf99dd8471bc3f3ca069e2da6
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-11-05 07:31:50 +0530

    Add files via upload

---
+++ int_func.py
@@ -0,0 +1,14 @@
+#num1= input("enter first number ")
+#num2=input("enter second number ")
+#sum= num1+ num2
+#print("total is "+ sum)# 54
+num1= int(input("enter first number "))
+num2=int(input("enter second number "))
+sum= num1+ num2
+print("total is "+ str(sum))#9
+num3=str(4)
+num4=int("45")
+num5=float("5")
+print(num4+num5)#if we add int number and float number the result present in float point
+
+
---
+++ shortcut.py
@@ -0,0 +1,5 @@
+# r is used for any escape sequence to normal form
+print("Line A \\n Line B")
+print(r"Line A\n Line B")
+print("\\\" \\'")
+print(r"\" \'")
