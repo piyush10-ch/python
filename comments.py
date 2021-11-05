5ef8bc95f6cffe9092dc67e062545b002c307931
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-11-05 07:31:11 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,11 @@
+print("hello\"world\" world")
+print('I\'m Piyush chaudhary')
+print ("line A")
+print("line B")
+print("line A\n line B\n line    C")
+print("name\tPiyush chaudhary")
+print("this is backlash\\")
+print ("this is double backlsh\\\\")
+print("this is hell\blo")
+# hash is used for comment in python for single line and double line is used one more #in second line
+
