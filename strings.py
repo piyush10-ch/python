cf7b5031742a7cf9632e96ea962a6f281a5a86c5
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-11-07 11:15:11 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,10 @@
+# strings is a collection of characters inside single and double quotes
+first_name ="Piyush"
+last_name="chaudhary"
+full_name=first_name+last_name
+print(full_name)
+#print(full_name + 3) error
+print(full_name + str(3))
+print(full_name + "3")
+print(first_name+"  " + last_name)
+print(full_name *5)
