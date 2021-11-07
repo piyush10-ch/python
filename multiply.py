b6a7802cd64703770bb2591856fa8ebd4fb9f520
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-11-07 11:13:23 +0530

    Add files via upload

---
+++ multiply.py
@@ -0,0 +1,2 @@
+num1,num2,num3=input("enter three number comma separated: ").split(",")
+print(f"the multiplicaton of numbers is :{(int(num1)*int(num2)*int(num3))} ")
---
+++ string_indexig.py
@@ -0,0 +1,12 @@
+#string indexing
+name ="PIYUSH"
+#p = 0     -6
+#i = 1     -5 
+#y = 2     -4
+#u = 3     -3
+#s = 4     -2
+#h = 5     -1
+print(name[4])
+print(name[2])
+print(name[-4])
+print(name[-2])
