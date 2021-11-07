bac5e9d01e121bf093a5fc559f9f20f9751ea46d
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-11-07 11:14:33 +0530

    Add files via upload

---
+++ string_slicing.py
@@ -0,0 +1,10 @@
+#STRING SLICING
+#syntax [start argument:stop argument-1]
+name="PIYUSH"
+print(name[2:5])
+print(name[0:2])
+print(name[2:4])
+print(name[1:3])
+print(name[:])
+print(name[2:])
+print(name[:4])
