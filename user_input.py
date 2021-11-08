d1b4ca0db8a7dd05dfd3e3cf355d324fd7c7fdc4
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-11-08 19:21:55 +0530

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,12 @@
+#user input is done by  input function 
+name=input("type your name ")
+age= input("your age? ")
+fname=input("your father name?")
+mname=input("your mother name?")
+#input fuction is always stored value in string
+print("hello"+" " + name  )
+print(age )
+print(fname)
+print(mname)
+
+
