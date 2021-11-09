22aa99309f5d08e874305a37933359a43a881454
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-11-09 15:45:00 +0530

    Add files via upload

---
+++ else_statement.py
@@ -0,0 +1,10 @@
+age = input("enter your age: ")
+age = int(age)
+if age >=14:
+    print("line A")
+    print("you are above 14")
+#if age==14:
+   # print("you are 14")    
+else:
+    print("sorry you are below 14")  
+      
