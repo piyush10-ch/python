c7e05d31e69553b52b628962d95e2b0c5903a8a4
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-11-09 15:45:37 +0530

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+#**********************print this following lines**********
+#this is \\ double backlash    
+# these are /\/\/\/\/\mountains
+#he is      awesome
+#\" \n \t \'
+print("this is \\\\ double backlash")
+print("these are /\\/\\/\\/\\/\\mountains" )
+print("he is \tawesome")
+print("\\\" \\n \\t \\\'")
+print("this is /\\/\\/\\/\\/\\/\\\\mountains")
