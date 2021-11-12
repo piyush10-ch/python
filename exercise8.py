44d6863344d3fc6114e6d3184c51c518896f2aa3
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-11-12 21:08:31 +0530

    Add files via upload

---
+++ exercise8.py
@@ -0,0 +1,12 @@
+# problem
+# ask user to input a number containing more than one digit 
+# example -1256
+# calculate 1+2+3+4 and print
+
+n = input("enter any number: ")
+n1 =int(n[0])
+n2=int(n[1])
+n3=int(n[2])
+n4=int(n[3])
+total=(n1+n2+n3+n4+len(n-1))
+print(total)
