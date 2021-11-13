e048319f2b0ff5be1dcd3e0876dccd500e5bda2d
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-11-13 20:15:10 +0530

    Add files via upload

---
+++ exercise6.py
@@ -0,0 +1,12 @@
+#EXERCISE- WATCH COCO
+#Ask user's name start with('a'or 'A') and age is above 10
+#print you can watch coco movie
+#else print sorry tou cannot watch coco
+name=input("Enter your name start with a or A :")
+age=input("enter your age: ")
+age=int(age)
+if (name[0]=='a' or name[0]=='A')    and age>10:
+    print("you can watch a movie")
+else:
+    print("sorry you can not watch movie") 
+     
