c8010a431cb0370eda61aaa1ed68a5a931bf98a1
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-11-13 20:15:49 +0530

    Add files via upload

---
+++ exercise7.py
@@ -0,0 +1,12 @@
+# exercise one of three
+#sum of n natural numbers
+# ask a user for natural number(n)
+# print total from 1 to n
+num = input(" enter any number :")
+num = int(num)
+total=0
+i = 1
+while i<=num:
+    total=total+i
+    i=i+1
+print(total)    
