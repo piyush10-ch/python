64e9ef766715a0d94cfd2f7e5f1616e85ee4419d
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-11-13 20:16:28 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,12 @@
+#print PIYUSH CHAUDHARY 15 times
+i = 1
+while i<=15:
+    print("PIYUSH CHAUHDRY")
+    i=i+1
+# for check the how many times loop run
+i =1
+while i<=10:
+    print(f"PIYUSH CHAUDHARY {i}")
+    i=i+1
+
+
