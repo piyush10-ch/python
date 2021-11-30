ae50aa892a1f2c3bb0db235f6cb4368c209f9c2e
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-11-30 19:24:04 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,15 @@
+number1= "Piyush" 
+print(number1)
+number1="chaudhary"
+print(number1)
+name = "jaat"
+print(name)
+name="123"
+print(name)
+# here number1,nmae are varables
+# rule of variables
+#we can never start variables with any number or any special char($)
+#we can only start with a letter or underscore(_)
+user_one_name="Piyush chaudhary"
+print(user_one_name)
+
