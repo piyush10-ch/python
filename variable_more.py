03bb6d0bfdbdb229836407db3dfdf44405971834
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-11-30 19:24:26 +0530

    Add files via upload

---
+++ variable_more.py
@@ -0,0 +1,8 @@
+name,age="Piyush chaudhary",23
+print(name,age)
+#print("hello" +" "+ name+"your age is"+" "+age)error because we cannot print int value with string so we make age string with help of double quotes
+age="23"
+print("hello"+" "+name+" "+ age)
+x=y=z=2
+print(x+y+z)#here we can also declare values in 3 variables
+
