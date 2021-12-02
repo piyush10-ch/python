95abc2454a3fcc6103d379fc14699e0840392f7a
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-12-02 16:37:39 +0530

    Add files via upload

---
+++ append_data.py
@@ -0,0 +1,11 @@
+# how to add items to our list
+#most common thing thsat you can do with your list
+fruits=['apple','mango']
+print(fruits)
+fruits.append('grapes')
+print(fruits)
+#in real life example we use append method 
+fruits=[]
+fruits.append('apple')
+fruits.append('banana')
+print(fruits)
