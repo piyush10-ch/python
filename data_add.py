c5b773b157ad559c771770a920675cca855c965e
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-12-02 16:40:28 +0530

    Add files via upload

---
+++ data_add.py
@@ -0,0 +1,15 @@
+#some more methods to add data in our list
+# insert method
+# how to join (concatenate) two list 
+# extend method
+# difference between aoppend and external method
+fruits1=['mango','apple']
+fruits1.insert(1,"grapes")
+print(fruits1)
+fruits2=['banana','orange']
+fruits=fruits1+fruits2
+print(fruits)
+#extend method is for to add more items in a single liust
+fruits1.extend(fruits2)
+print(fruits1)
+print(fruits2)
