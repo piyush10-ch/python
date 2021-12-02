1948c0000177b54f8eb4584a44277c1006c693fb
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-12-02 16:41:22 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,15 @@
+numbers=[1,2,3,4]
+print(numbers)
+words=["piyush",'arpit',"aayush",]
+print(words)
+mixed=[1,2,3,4,"piyush","arpit",'aayush']
+print(mixed)
+#we can print anythimg in lists like int,float,string
+#for change in list
+fruits=["mango","banana","apple","orange","grapes"]
+fruits[2]="piyush"
+print(fruits)#here index of '2' change
+#FOR CHANGE OF LIST
+name=["ram","shyam","john","radhe"]
+name[1:]=["piyush","aayush","arpit"]
+print(name)
