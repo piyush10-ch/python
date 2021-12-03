fcaafccd9b641890c80567a89d509892bfb76f74
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-12-03 15:10:04 +0530

    Add files via upload

---
+++ compare_lists.py
@@ -0,0 +1,9 @@
+# compare lists
+# is vs ==
+fruits=['apple', 'mango','grapes']
+fruits1=['apple','peer', 'grapes']
+fruits2=['aaple','peer','orange','pineapple']
+fruits3=['apple', 'mango','grapes']
+print(fruits==fruits1)
+print(fruits==fruits3)
+print(fruits is fruits3)
