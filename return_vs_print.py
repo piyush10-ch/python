61934e063df63393b9bb257b98efb277167169b4
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-12-05 20:27:57 +0530

    Add files via upload

---
+++ return_vs_print.py
@@ -0,0 +1,10 @@
+def add_three(a,b,c):
+     return a+b+c
+print(add_three(5,5,5))
+def add_three(a,b,c):
+    print(a+b+c)
+add_three(5,5,5)
+
+#both have same output  but in print we don't need to write a print statement
+
+
